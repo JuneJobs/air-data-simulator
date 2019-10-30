2e77f0b13019f21ce4592ddfd6291e6b14f351f9
junejobs <j.jobs1028@gmail.com>
2019-10-30 16:48:14 +0900

    Update Client resources

---
+++ Client/src/App.js
@@ -0,0 +1,15 @@
+import React, {
+  Component
+} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Main from './routes/Main';
+
+class App extends Component {
+  render() {
+    return ( < Main / >
+    );
+  }
+}
+
+export default App;
