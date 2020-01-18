1fdcb56f3c2f8cc40ed9312fa3b0889c113dd18d
junejobs <j.jobs1028@gmail.com>
2020-01-18 10:23:12 +0900

    Update Air Simulator request Url

--- Client/src/routes/AirSensorSimulator/AirSensorSimulator.js
+++ Client/src/routes/AirSensorSimulator/AirSensorSimulator.js
@@ -22,7 +22,7 @@ const generator = new codeGenerator();
 
 //axios.defaults.baseURL = 'http://intuseer.co.kr:8001';
 //axios.defaults.baseURL = 'http://somnium.me:1234';
-const simulator_server = 'http://localhost:60031';
+const simulator_server = config.api_simulator;
 //const simulator_server = config.api_simulator;
 //const api_server = 'http://localhost:8001';
 //const simulator_server = 'http://sv.somnium.me:60031';
